65e8400fbf41f26dc0e3bb0ce16a92cdeb42800c
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-06-01 00:59:53 +0200

    feat: disable scroll

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center gap-8 overflow-hidden ">
         <BingoTitle />
         <BingoBoard />
       </div>
