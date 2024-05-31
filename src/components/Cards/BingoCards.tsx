56b03e33432f385d99744e3c42244bc2c5b0cbfb
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-06-01 00:23:42 +0200

    feat: adjust winning condition for 5x5

--- src/components/Cards/BingoCards.tsx
+++ src/components/Cards/BingoCards.tsx
@@ -2,21 +2,34 @@ import { useGetBingoData } from "@/actions/get-bingo-data";
 import BingoCard from "./BingoCard";
 import { useEffect } from "react";
 import { useToast } from "../ui/use-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBingo } from "@/actions/update-bingo";
 
 type Props = {
   data: TBingoCard[];
 };
+
 function BingoCards({ data }: Props) {
   const { data: statusData } = useGetBingoData();
   const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: updateBingoMutation } = useMutation({
+    mutationFn: updateBingo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["bingo-data"] });
+    },
+  });
 
   useEffect(() => {
     for (let i = 0; i < data.length; i++) {
       const card = data[i];
       card.status = statusData[card.column];
     }
+    console.log(data);
 
     if (checkWin(data)) {
+      updateBingoMutation({ column: "completed", checked: false });
       toast({
         title: "Bingo!",
         description: "You have a bingo!",
@@ -27,9 +40,9 @@ function BingoCards({ data }: Props) {
 
   function checkWin(grid: any): boolean {
     // Check rows
-    for (let row = 0; row < 4; row++) {
-      const startIndex = row * 4;
-      const rowValues = grid.slice(startIndex, startIndex + 4);
+    for (let row = 0; row < 5; row++) {
+      const startIndex = row * 5;
+      const rowValues = grid.slice(startIndex, startIndex + 5);
 
       if (rowValues.every((cell: any): any => cell.status)) {
         return true; // Full row found
@@ -37,10 +50,10 @@ function BingoCards({ data }: Props) {
     }
 
     // Check columns
-    for (let col = 0; col < 4; col++) {
+    for (let col = 0; col < 5; col++) {
       const colValues: string[] = [];
-      for (let row = 0; row < 4; row++) {
-        colValues.push(grid[row * 4 + col]);
+      for (let row = 0; row < 5; row++) {
+        colValues.push(grid[row * 5 + col]);
       }
       if (colValues.every((cell: any): any => cell.status)) {
         return true; // Full column found
@@ -48,8 +61,20 @@ function BingoCards({ data }: Props) {
     }
 
     // Check diagonals
-    const diagonal1: string[] = [grid[0], grid[5], grid[10], grid[15]];
-    const diagonal2: string[] = [grid[3], grid[6], grid[9], grid[12]];
+    const diagonal1: string[] = [
+      grid[0],
+      grid[6],
+      grid[12],
+      grid[18],
+      grid[24],
+    ];
+    const diagonal2: string[] = [
+      grid[4],
+      grid[8],
+      grid[12],
+      grid[16],
+      grid[20],
+    ];
     if (
       diagonal1.every((cell: any): any => cell.status) ||
       diagonal2.every((cell: any): any => cell.status)
@@ -68,7 +93,7 @@ function BingoCards({ data }: Props) {
     );
 
   return (
-    <div className="grid grid-cols-4 gap-2 p-2 m-2 border rounded-lg sm:p-8 sm:gap-6 bg-amber-50 border-amber-400">
+    <div className="grid grid-cols-5 gap-2 p-2 m-2 bg-gray-300 border border-gray-400 rounded-xl sm:p-8 sm:gap-4">
       {data.map((card: TBingoCard) => (
         <BingoCard
           key={card.title}
