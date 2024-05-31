47d4b1eaf5ab9d793b0e0121f67c4d54b7686920
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-06-01 01:00:04 +0200

    feat: update title

--- src/components/Titles/BingoTitle.tsx
+++ src/components/Titles/BingoTitle.tsx
@@ -2,19 +2,13 @@
 
 function BingoTitle() {
   const today = new Date().toISOString().slice(0, 10);
-  // const [streak, setStreak] = useState(0);
 
   return (
     <div className="mt-16">
       <h1 className="mb-6 text-4xl font-bold text-center sm:text-6xl">
-        Discord Bingo
+        Discord Bingo 🎰
       </h1>
       <div className="flex flex-col items-center justify-between gap-4 sm:justify-center sm:flex-row">
-        {/* <div className="flex gap-1">
-          <p className="font-semibold">Current streak:</p>
-          <p>0</p>
-          <p className="animate-wiggle ">🔥</p>
-        </div> */}
         <div className="flex gap-1">
           <p className="font-semibold">Today is:</p>
           <p>{today}</p>
